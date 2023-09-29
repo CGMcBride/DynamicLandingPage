8074ba73660b600027e7131b567e77c8738f0aa1
Conner McBride <connermcbride@ymail.com>
2023-09-29 14:24:19 -0500

    updated code for page

---
+++ js/config.js
@@ -0,0 +1,6 @@
+/*export const backgroundImages = {
+    morning: "url('C:\Users\Conner\Documents\GitHub\DynamicLandingPage\js\img\IMG_0388.jpg')",
+    afternoon: "url('C:\Users\Conner\Documents\GitHub\DynamicLandingPage\js\img\IMG_0394.jpg')",
+    evening: "url('C:\Users\Conner\Documents\GitHub\DynamicLandingPage\js\img\IMG_3002(Edited).jpg')"
+    
+};
--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 // Document Object Model (DOM) Elements
+//import { backgroundImages } from './config.js';
 const time= document.getElementById('time'),
 greeting = document.getElementById('greeting'),
 name = document.getElementById('name'),
@@ -10,7 +11,7 @@ function showTime(){
     hour = today.getHours(),
     min = today.getMinutes(),
     sec = today.getSeconds(),
-     month = today.getMonth(),
+    month = today.toLocaleString('default', { month: 'long' }),
      year = today.getFullYear();
 
     // Set AM or PM
@@ -20,8 +21,9 @@ function showTime(){
     hour = hour % 12 || 12;
 
     // Output the time
-    time.innerHTML= `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
-    
+    time.innerHTML= `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}<span> ${amPm}</span><br>${month} ${year}`;    
+
+   // setBackGr(); // Call setBackGr function to update background image and greeting
 
     setTimeout(showTime,1000);
 }
@@ -33,23 +35,40 @@ function addZero(n){
 
 // Set Background and Greeting
 function setBackGr(){
+    let today = new Date(),
+    hour = today.getHours();
+
+    if(hour < 12 ){
+        // Morning
+        document.body.style.backgroundImage = "url('..img/IMG_0388.jpg')";
+        greeting.textContent = 'Good Morning'
+    } else if( hour < 18){
+        // Afternoon
+        document.body.style.backgroundImage = "url('https://www.google.com/url?sa=i&url=https%3A%2F%2Ffreenaturestock.com%2Fbright-afternoon%2F&psig=AOvVaw106_5olLhDTNkZV4ps1cYo&ust=1696099979712000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCLD9wfi-0IEDFQAAAAAdAAAAABAE')";
+        greeting.textContent = 'Good Afternoon'
+    } else {
+        //  Evening
+        document.body.style.backgroundImage = "url('..img/IMG_3002(Edited).jpg')";
+        greeting.textContent = 'Good Evening'
+    }
+}
+/*function setBackGr(){
     let currentDate = new Date(),
     currentHour = currentDate.getHours();
 
     if(currentHour < 12 ){
         // Morning
-        document.body.style.backgroundImage = "url('img\IMG_0388.jpg')";
+        document.body.style.backgroundImage = backgroundImages.morning;
         greeting.textContent = 'Good Morning'
-    } else if( hour < 18){
+    } else if( currentHour < 18){
         // Afternoon
-        document.body.style.backgroundImage = "url('img\IMG_0394.jpg')";
+        document.body.style.backgroundImage = backgroundImages.afternoon;
         greeting.textContent = 'Good Afternoon'
     } else {
         //  Evening
-        document.body.style.backgroundImage = "url('img\IMG_3002(Edited).jpg')";
+        document.body.style.backgroundImage = backgroundImages.evening;
         greeting.textContent = 'Good Evening'
     }
-}
-
+}*/
 // Run
 showTime();
